d1b6a074c309bf490b00ef5bd23821c45303cff6
Alex Haible <52239823+AlexHaible@users.noreply.github.com>
2025-12-05 02:44:56 +0100

    Bit nicer UI stuff.

--- resources/views/livewire/run-command.blade.php
+++ resources/views/livewire/run-command.blade.php
@@ -9,11 +9,29 @@
     </button>
 
     @if ($filePlaceholder !== 'No input file required')
-        <input
-            type="file"
-            wire:model="uploadedFile"
-            class="text-xs"
-            accept=".txt"
-        >
+        <label class="relative cursor-pointer px-3 py-1.5 rounded bg-gray-700 text-white text-xs hover:bg-gray-600">
+            <span>
+                @if($uploadedFile)
+                    {{ $uploadedFile->getClientOriginalName() }}
+                @else
+                    No file picked
+                @endif
+            </span>
+            <input
+                type="file"
+                wire:model="uploadedFile"
+                class="absolute inset-0 opacity-0 cursor-pointer"
+                accept=".txt"
+            >
+        </label>
+        @if($uploadedFile)
+            <button
+                type="button"
+                wire:click="$set('uploadedFile', null)"
+                class="px-2 py-1 rounded bg-red-600 text-white text-xs hover:bg-red-700"
+            >
+                Clear
+            </button>
+        @endif
     @endif
 </div>
